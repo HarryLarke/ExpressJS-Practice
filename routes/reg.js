ad576c4195dc76ffbf3cce02799182b734fc5e71
HarryLarke <harrylarke1@hotmail.com>
2025-03-08 20:45:36 +0000

    server running

---
+++ routes/reg.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router() 
+const path = require('path')
+
+router.get('^/$|/reg(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'reg.html'))
+})
+
+module.exports = router 
--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log("Hello World!")
+const express = require('express')
+const app = express() 
+const path = require('path')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const credentials = require('./middleware/credentials')
+const PORT = process.env.PORT || 3500
+
+app.use(credentials)
+app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use('/', require('./routes/root'))
+app.use('/', require('./routes/login'))
+app.use('/', require('./routes/reg'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }
+    if(req.accepts('json')) {
+        res.json({error: "404 Not Found!"})
+    }
+    else {
+        res.type('txt').send('404 Not Found!')
+    }
+})
+
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
