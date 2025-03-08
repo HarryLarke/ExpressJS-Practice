9d86002b84d4e7a24487c99525bdc7812e878bef
HarryLarke <harrylarke1@hotmail.com>
2025-03-08 22:18:16 +0000

    loggers added

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require('path')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
 const credentials = require('./middleware/credentials')
+const { logger } = require('./middleware/logEvents')
+const logError = require('./middleware/logError')
 const PORT = process.env.PORT || 3500
 
+app.use(logger)
 app.use(credentials)
 app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: false}))
@@ -14,20 +17,22 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/root'))
-app.use('/', require('./routes/login'))
 app.use('/', require('./routes/reg'))
+app.use('/', require('./routes/login'))
 
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
     }
-    if(req.accepts('json')) {
+    else if(req.accepts('json')) {
         res.json({error: "404 Not Found!"})
     }
     else {
         res.type('txt').send('404 Not Found!')
     }
 })
+
+app.use(logError)
 
 app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
